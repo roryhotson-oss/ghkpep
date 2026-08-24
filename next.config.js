4af6bc1e47a73eb5d26cf1298a8528354198c95f
Trident <roryhotson@gmail.com>
2026-08-24 21:36:22 +0100

    Harden Vercel preview SEO

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: '.*\\.vercel\\.app' }],
+        headers: [{ key: 'X-Robots-Tag', value: 'noindex' }],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
